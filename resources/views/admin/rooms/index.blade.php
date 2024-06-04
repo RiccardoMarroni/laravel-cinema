f648f489c8b995057e12f98fb6fc95bed865e1d9
RiccardoMarroni <marroni.riccardo@gmail.com>
2024-06-04 12:51:25 +0200

    Co-authored-by: eldin001 <eldin001@users.noreply.github.com> Co-authored-by: adrisala20 <adrisala20@users.noreply.github.com>

--- resources/views/admin/rooms/index.blade.php
+++ resources/views/admin/rooms/index.blade.php
@@ -1,54 +1,34 @@
-@extends('layouts.admin')
-@section('title', 'Rooms')
+@extends('layouts.app')
 
 @section('content')
- <div class="container">
- <table class="table table-striped">
-        <thead>
-            <tr>
-              <th scope="col">Id</th>
-              <th scope="col">Name</th>
-              <th scope="col">Seats Number</th>
-              <th scope="col">I sense</th>
-              <th scope="col">Base Price</th>
-              <th scope="col">Image</th>
-              <th scope="col">Created At</th>
-              <th scope="col">Update At</th>
-              <th scope="col">Action</th>
-            </tr>
-          </thead>
-          <tbody>
-            @foreach ($rooms as $room)
-            <tr>
-                <td>{{$room->id}}</td>
-                <td>{{$room->name}}</td>
-                <td>{{$room->seats_num}}</td>
-                <td>{{$room->isense}}</td>
-                <td>{{$room->base_price}}</td>
-                <td>{{$room->image}}</td>
-                <td>{{$room->created_at}}</td>
-                <td>{{$room->updated_at}}</td>
-
-                {{-- <td>
-                    <a href="{{route('admin.rooms.show', $room->slug)}}" title="Show" class="text-black px-2"><i class="fa-solid fa-eye"></i></a>
-                    <a href="{{route('admin.rooms.edit', $room->slug)}}" title="Edit" class="text-black px-2"><i class="fa-solid fa-pen"></i></a>
-                    <form action="{{route('admin.rooms.destroy', $room->slug)}}" method="POST" class="d-inline-block">
-                      @csrf
-                      @method('DELETE')
-                      <button type="submit" class="delete-button border-0 bg-transparent"  data-item-title="{{ $room->title }}" data-item-id = "{{ $room->id }}">
-                        <i class="fa-solid fa-trash"></i>
-                      </button>
-
-                    </form>
-
-
-                </td> --}}
-              </tr>
-            @endforeach
-
-
-          </tbody>
-      </table>
-
- </div>
+    <h1>Sale</h1>
+    <a href="{{ route('rooms.create') }}">Crea nuova sala</a>
+    <table>
+        <tr>
+            <th>Nome</th>
+            <th>Posti</th>
+            <th>Isense</th>
+            <th>Prezzo Base</th>
+            <th>Immagine</th>
+            <th>Azioni</th>
+        </tr>
+        @foreach ($rooms as $room)
+        <tr>
+            <td>{{ $room->name }}</td>
+            <td>{{ $room->seats_num }}</td>
+            <td>{{ $room->isense ? 'Sì' : 'No' }}</td>
+            <td>{{ $room->base_price }}</td>
+            <td><img src="{{ $room->image }}" alt="{{ $room->name }}" width="100"></td>
+            <td>
+                <a href="{{ route('rooms.show', $room->id) }}">Mostra</a>
+                <a href="{{ route('rooms.edit', $room->id) }}">Modifica</a>
+                <form action="{{ route('rooms.destroy', $room->id) }}" method="POST" style="display:inline;">
+                    @csrf
+                    @method('DELETE')
+                    <button type="submit">Elimina</button>
+                </form>
+            </td>
+        </tr>
+        @endforeach
+    </table>
 @endsection
